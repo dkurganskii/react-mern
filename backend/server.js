d55163383bd7513bcdc3ec5088ff821c8c07e1e2
dkurganskii <44755635+dkurganskii@users.noreply.github.com>
2024-02-08 08:29:49 -0600

    implement auth middleware

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import cookieParser from 'cookie-parser';
 
 dotenv.config()
 const port = process.env.PORT || 5022;
@@ -15,6 +16,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser()); 
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
